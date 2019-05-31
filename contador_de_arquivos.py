84540620e66c8bf1a1ebae89f536eb72ab4c241a
frclasso <frclasso@yahoo.com.br>
2019-05-31 12:29:33 -0300

    changing the name

--- contador_de_arquivos.py
+++ contador_de_arquivos.py
@@ -2,8 +2,8 @@
 
 import glob
 
-print(len(list(glob.iglob("/Users/fabio/Estudo/Prog/Python/apostila_python_modulo_2/*/*.py", recursive=True))))
+print(len(list(glob.iglob("/Users/fabio/Estudo/Prog/Python/2nd_Step_Python_Fabio_Classo/*/*.py", recursive=True))))
 
 print()
-print(sum(1 for i in glob.iglob("/Users/fabio/Estudo/Prog/Python/apostila_python_modulo_2/*.py")))
+print(sum(1 for i in glob.iglob("/Users/fabio/Estudo/Prog/Python/2nd_Step_Python_Fabio_Classo/*.py")))
 
