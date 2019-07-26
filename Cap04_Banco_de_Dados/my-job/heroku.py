8495f866c7b9355f3bcdfcbaaf9dd77e918910de
frclasso <frclasso@yahoo.com.br>
2019-07-26 08:15:23 -0300

    heroku anotacoes

--- Cap04_Banco_de_Dados/my-job/heroku.py
+++ Cap04_Banco_de_Dados/my-job/heroku.py
@@ -15,7 +15,7 @@
 #--------------
 # criando aplicacao Heroku
 # heroku create 'myapp'
-# heroku open - abre a url no browser
+# heroku open -a simuladorapp- abre a url no browser
 
 # deploying
 # git push heroku master
@@ -24,9 +24,10 @@
 # instalando heroku database
 #heroku addons
 
+#heroku addons:create heroku-postgresql:hobby-dev -a simuladorapp
 # heroku addons:create heroku-postgresql:hobby-dev     # hobby-dev is free
 
-# heroku pg # exibe dados da database
+# heroku pg -a 'nomedo app'# exibe dados da database
 
 
 # configurando as urls da database automaticamente
